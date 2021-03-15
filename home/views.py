d53084491c6cf157688bf07da413639d7bf24398
BigB0y95 <phanquang199x@gmail.com>
2021-03-15 18:02:58 +0700

    update

--- home/views.py
+++ home/views.py
@@ -3,3 +3,7 @@ from django.http import HttpResponse
 # Create your views here.
 def index(request):
     return render(request, 'pages/home.html')
+def python_page(request):
+    return render(request, 'pages/python_page.html')
+def home(request):
+    return render(request, 'pages/home.html')
